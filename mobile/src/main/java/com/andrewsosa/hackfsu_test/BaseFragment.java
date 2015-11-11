de21f6af3bde658a97b3b599a33ae7c010795888
Andrew Sosa <sosa@cs.fsu.edu>
2015-11-10 22:39:30 -0500

    Minor layout changes, moved adapters.

---
+++ mobile/src/main/java/com/andrewsosa/hackfsu_test/BaseFragment.java
@@ -0,0 +1,28 @@
+package com.andrewsosa.hackfsu_test;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by andrewsosa on 11/6/15.
+ */
+public class BaseFragment extends Fragment {
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+
+        void registerToolbar(Toolbar toolbar);
+
+    }
+
+}
---
+++ mobile/src/main/java/com/andrewsosa/hackfsu_test/UpdateFragment.java
@@ -0,0 +1,127 @@
+package com.andrewsosa.hackfsu_test;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class UpdateFragment extends BaseFragment {
+
+    RecyclerView mRecyclerView;
+    LinearLayoutManager mLayoutManager;
+
+    // TODO: Rename and change types and number of parameters
+    public static UpdateFragment newInstance() {
+        UpdateFragment fragment = new UpdateFragment();
+        return fragment;
+    }
+
+    public UpdateFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_update, container, false);
+
+
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.my_recycler_view);
+
+
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        // specify an adapter (see also next example)
+        UpdatesRecyclerAdapter mAdapter = new UpdatesRecyclerAdapter(new String[]{"Hello",
+                "World", "HackFSU","World", "HackFSU","World", "HackFSU","World", "HackFSU"});
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //mRecyclerView.scrollToPosition(0);
+    }
+
+
+    // Adapter used by this fragment
+    private static class UpdatesRecyclerAdapter extends
+            RecyclerView.Adapter<UpdatesRecyclerAdapter.ViewHolder> {
+
+        private String[] mDataset;
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            public View card;
+            public TextView mTextView;
+            public ViewHolder(View v) {
+                super(v);
+                card = v;
+                mTextView = (TextView) v.findViewById(R.id.update_title);
+            }
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public UpdatesRecyclerAdapter(String[] myDataset) {
+            mDataset = myDataset;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public UpdatesRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                             int viewType) {
+            // create a new view
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.update, parent, false);
+
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            holder.mTextView.setText(mDataset[position]);
+
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
+    }
+}
