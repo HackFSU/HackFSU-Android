ee663cfd2a6c284817675cf135e2b4d1bac179e3
Unknown <randybruno424@gmail.com>
2018-01-11 20:04:41 -0500

    Revert "Revert "Tabs & Primitive Judging Structure""
    This reverts commit 957c22d4c2ed4e2f80f78d7cda5449f9c83a2e94.

---
+++ mobile/src/main/java/com/hackfsu/mobile/android/app/adapter/fragment/ProfileFragment.java
@@ -0,0 +1,42 @@
+package com.hackfsu.mobile.android.app.adapter.fragment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.hackfsu.mobile.android.api.API;
+import com.hackfsu.mobile.android.api.model.SponsorModel;
+import com.hackfsu.mobile.android.app.R;
+//import com.parse.FindCallback;
+//import com.parse.GetDataCallback;
+//import com.parse.ParseClassName;
+//import com.parse.ParseException;
+//import com.parse.ParseFile;
+//import com.parse.ParseImageView;
+//import com.parse.ParseObject;
+//import com.parse.ParseQuery;
+import com.squareup.picasso.Picasso;
+import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ProfileFragment extends BaseFragment {
+
+
+
+
+}
